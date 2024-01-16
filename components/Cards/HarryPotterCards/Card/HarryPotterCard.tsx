2c2b0e90a8e68835c147a0a25054c39c0983b446
alnikif <nikiforiakoleksandr@gmail.com>
2024-01-16 14:43:03 +0100

    add Harry Potter page

--- components/Cards/HarryPotterCards/Card/HarryPotterCard.tsx
+++ components/Cards/HarryPotterCards/Card/HarryPotterCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HarryPotterType } from '../../../../types/harryPotterTypes';
 import styles from './HarryPotterCard.module.scss';
-import { Link } from 'react-router-dom';
+import Link from 'next/link';
 
 export const HarryPotterCard: React.FC<{ readonly characterData: HarryPotterType }> = ({ characterData }) => {
   const { id, image, name, gender, species, wizard } = characterData;
@@ -9,7 +9,7 @@ export const HarryPotterCard: React.FC<{ readonly characterData: HarryPotterType
     <div className={styles.CharacterCard}>
       <img src={image} alt={name} />
       <div>
-        <Link to={`/harry-potter/${id}`} className={styles.link}>
+        <Link href={`/harry-potter/${id}`} className={styles.link}>
           {name}
         </Link>
       </div>
